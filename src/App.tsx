59ecc2bb92a782108a8eda7ab5505ce1b4701f85
Mamanov Emil <emil.mamanov97@gmail.com>
2023-11-19 22:38:33 +0600

    added component added axios added function for receiving a messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MessageList from './components/MessageList/MessageList';
+import { Message } from './types';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_URL = 'http://146.185.154.90:8000/messages';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App: React.FC = () => {
+    const [messages, setMessages] = useState<Message[]>([]);
 
-export default App
+    useEffect(() => {
+        const intervalId = setInterval(fetchMessages, 5000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const fetchMessages = async () => {
+        try {
+            const response = await axios.get(API_URL);
+            setMessages(response.data);
+        } catch (error) {
+            console.error('Error fetching messages:', error);
+        }
+    };
+
+
+    return (
+        <div>
+            <h1>React Chat App</h1>
+            <div className="chat-block">
+                <MessageList messages={messages} />
+            </div>
+        </div>
+    );
+};
+
+export default App;
